cbf9301eacd9a24d10434ac40ed75715b3b55aa9
Kbot64 <ktsky515@gmail.com>
2025-11-06 15:25:32 -0600

    Added game loop and display for background

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 from constants import *
+from logger import log_state
+
 
 def main():
+    pygame.init()
+    print(type(SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
