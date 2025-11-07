7f7522870c2c67b82ce56859b8aae427db48b085
Kbot64 <ktsky515@gmail.com>
2025-11-06 19:43:10 -0600

    Added player and other classes for character sprite

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print(type(SCREEN_HEIGHT))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         log_state()
         screen.fill("black")
+        # print("Entering screen")    
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
